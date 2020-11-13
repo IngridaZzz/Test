aa101901d504460d35d061eed9360e21ef34eb36
Ingrida Zalyte <33633254+IngridaZzz@users.noreply.github.com>
2020-11-13 13:30:19 +0100

    publishhh

---
+++ Frontend_Mandatory_1/js/moviesJSON.js
@@ -0,0 +1,20 @@
+{
+  "movies": [
+    {
+         "title": "Blade Runner 2049",
+         "youtubeId": "gCcx85zbxz4&ab&ab"
+    },
+    {
+        "title": "Interstellar",
+        "youtubeId": "zSWdZVtXT7E&ab"
+    },
+    {
+        "title": "Lion King",
+        "youtubeId": "7TavVZMewpY&ab"
+    },
+    {
+        "title": "Inception",
+        "youtubeId": "YoHD9XEInc0&ab"
+    }
+  ]
+}
